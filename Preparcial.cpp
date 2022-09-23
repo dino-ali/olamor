0a5e520a0d9fdc2daadefb0a319e762d8970506f
davinchooo <112956569+davinchooo@users.noreply.github.com>
2022-09-22 21:31:24 -0500

    Update Preparcial.cpp

--- Preparcial.cpp
+++ Preparcial.cpp
@@ -1,54 +1,11 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-
-void two_smallest( vector <string > & arr){
-    int minimo, x;
-    
-    minimo = arr[0].length();
-    for (int i = 1; i < arr.size(); i++){
-        if (arr[i].length() <= minimo){
-            minimo = arr[i].length();
-            x = i;
-        }
-    }
-    cout << "El  primero mas pequeño es "<< arr[x] << endl;
-    arr.erase(arr.begin()+ x);
-    minimo = arr[0].length();
-    
-    for (int i = 1; i < arr.size(); i++){
-        if (arr[i].length() <= minimo){
-            minimo = arr[i].length();
-            x = i;
-        }
-    }
-    
-    cout << "El  segundo mas pequeño es "<< arr[x] << endl;    
-}
-
-
-int main()
-{
-    vector<string> v;
-    v.push_back("ABC");    
-    v.push_back("2");
-    v.push_back("AB");
-    v.push_back("4");
-    v.push_back("F");
-    v.push_back("A");
-
-
-    
-    
-    for (int i = 0; i < v.size(); i++){
-        cout << v[i] << " ";
-    }
-    cout<<endl;
+void bubble_sort(vector <string > & v){
     int minimo=v[0].length();
     int a;
-    string b;
+    string b,c;
     for(int i=0; i< v.size(); i++){
         for( int j=i; j<v.size(); j++){
             if(v[j].length() <= minimo){
@@ -61,14 +18,6 @@ int main()
         v[a] = b;
         minimo = 100000000;
     }
-    for (int i = 0; i < v.size(); i++){
-        cout << v[i] << " ";
-    }
-    
-    cout << endl;
-    
-
-    string c;
     for(int i=0; i< v.size(); i++){
         for( int j=1; j<v.size(); j++){
             if(v[j-1].length() == v[j].length()){
@@ -88,5 +37,21 @@ int main()
     }
     for (int i = 0; i < v.size(); i++){
         cout << v[i] << " ";
-    }
+    }   
 }
+
+int main()
+{
+    vector<string> v;
+    v.push_back("ABC");    
+    v.push_back("2");
+    v.push_back("AB");
+    v.push_back("4");
+    v.push_back("F");
+    v.push_back("A");    
+    
+    bubble_sort(v);
+
+    return 0;
+}
+
