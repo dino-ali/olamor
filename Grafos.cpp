6f694d6d70e22ba7222046e847d20cd7e6cc7270
davinchooo <112956569+davinchooo@users.noreply.github.com>
2022-11-09 16:18:07 -0500

    Update Grafos.cpp

--- Grafos.cpp
+++ Grafos.cpp
@@ -1,450 +1,65 @@
-
-// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
-
 #include<iostream>
 
 using namespace std;
 
-int insertar(int* v, int n, int x, int pos){
-    for(int i = n; i>pos;i--){
-        v[i] = v[i-1];
-    }
-    v[pos] = x;
-   return n+1; 
-}
+class Grafos{
+    int** mat;
+public:
 
-int eliminar(int* v, int n, int pos){
-    for(int i = pos; i<n-1;i++){
-        v[i] = v[i+1];
-    }
-   return n-1; 
-}
-
-int* genvect(int n){
-    int* v = new int[n];
-    for(int i=0; i<n; i++){
-        v[i] = rand()%100;
-    }
-    return v;
-}
-
-void printMat(int** mat, int n, int m){
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            cout<<mat[i][j]<<"\t";
-       }
-       cout<<"\n";
+    Grafos(){
+        mat = new int*[0];
     }
     
-}
-
-void initMat(int** mat, int n, int m){
-   for(int i=0; i<n; i++){
-       for(int j=0; j<m; j++){
-           mat[i][j] = 0;
-       }
-   }
+    Grafos(int p){
+        mat = new int*[p];
+    }
     
-}
+    int** getMat(){
+      return mat;
+    }
+    
+    void setMat(int** mt){
+      mat =  mt;
+    }
+    
+    void initMat(int p){
+        for(int i = 0; i < p; ++i){
+            mat[i] = new int[p];   
+        } 
+        
+        for(int i = 0; i < p; i++){
+            for(int j = 0; j < p; j++){
+               mat[i][j] = 0;
+            }
+        }
+    }
+    
+    void printMat(int p){
+        for(int i = 0; i<p; i++){
+            for(int j = 0; j<p; j++){
+                cout<<mat[i][j]<<"\t";
+           }
+           cout<<"\n";
+        }
+        
+    }    
+    
+    void addEnlace(int i, int f, int p){
+        mat[i-1][f-1] = p;
+    }
 
+};
 
 int main()
 {
-   int p;
-   cout << "Cuantos nodos son: ";
-   cin>>p;
-   int** mat = new int*[p];
-   int x;
-   
-   for(int i = 0; i < p; ++i)
-        mat[i] = new int[p];
-   
-   initMat(mat,p,p);
-
-   cout<<"\n";
-   
-   printMat(mat, p,p);
-   
-   cout<<"\n";
-   
-   
-   for(int i=0;i<p;i++)
-    delete[] mat[i];
-
-    delete[] mat;
-   
+    int p, x;
+    cout << "Cuantos nodos son: ";
+    cin>>p;
+    Grafos g = Grafos(p);
+    g.initMat(p);
+    g.printMat(p);
+    g.addEnlace(3,2,15);
+    cout<< endl << endl;
+    g.printMat(p);
     return 0;
 }
-/*
-----------------------------------------------------------------
-
-/*
-  Name: Representacion enlazada de un grafo
-  Author:   Joel Fernandez
-  Date: 01/12/12 23:43
-  Web: http://codebotic.blogspot.com
-  Description: programa que muestra un grafo en su forma enlazada
-*/
-#include<iostream>
-#include<conio.h>
-using namespace std;
-
-
-struct nodo{
-            char nombre;//nombre del vertice o nodo
-            struct nodo *sgte;
-            struct arista *ady;//puntero hacia la primera arista del nodo
-            };
-struct arista{
-              struct nodo *destino;//puntero al nodo de llegada
-              struct arista *sgte;
-              };
-typedef struct nodo *Tnodo;//  Tipo Nodo
-typedef struct arista *Tarista; //Tipo Arista
-
-Tnodo p;//puntero cabeza
-
-void menu();
-void insertar_nodo();
-void agrega_arista(Tnodo &, Tnodo &, Tarista &);
-void insertar_arista();
-void vaciar_aristas(Tnodo &);
-void eliminar_nodo();
-void eliminar_arista();
-void mostrar_grafo();
-void mostrar_aristas();
-
-
-/*                        Funcion Principal
----------------------------------------------------------------------*/
-int main(void)
-{   p=NULL;
-    int op;     // opcion del menu
-
-
-    system("color 0b");
-
-    do
-    {
-        menu();
-        cin>>op;
-
-        switch(op)
-        {
-            case 1:
-                    insertar_nodo();
-                    break;
-            case 2: insertar_arista();
-                    break;
-            case 3: eliminar_nodo();
-                    break;
-            case 4: eliminar_arista();
-                    break;
-            case 5: mostrar_grafo();
-                    break;
-            case 6: mostrar_aristas();
-                    break;
-
-            default: cout<<"OPCION NO VALIDA...!!!";
-                     break;
-
-
-        }
-
-        cout<<endl<<endl;
-        system("pause");  system("cls");
-
-    }while(op!=7);
-    getch();
-    return 0;
-}
-
-/*                          Menu
----------------------------------------------------------------------*/
-void menu()
-{
-    cout<<"\n\tREPRESENTACION DE GRAFOS DIRIGIDOS\n\n";
-    cout<<" 1. INSERTAR UN NODO                 "<<endl;
-    cout<<" 2. INSERTAR UNA ARISTA              "<<endl;
-    cout<<" 3. ELIMINAR UN NODO                 "<<endl;
-    cout<<" 4. ELIMINAR UNA ARISTA              "<<endl;
-    cout<<" 5. MOSTRAR  GRAFO                   "<<endl;
-    cout<<" 6. MOSTRAR ARISTAS DE UN NODO       "<<endl;
-    cout<<" 7. SALIR                            "<<endl;
-
-    cout<<"\n INGRESE OPCION: ";
-}
-
-/*                      INSERTAR NODO AL GRAFO
----------------------------------------------------------------------*/
-void insertar_nodo()
-{
-    Tnodo t,nuevo=new struct nodo;
-    cout<<"INGRESE VARIABLE:";
-    cin>>nuevo->nombre;
-    nuevo->sgte = NULL;
-    nuevo->ady=NULL;
-
-    if(p==NULL)
-     {
-        p = nuevo;
-        cout<<"PRIMER NODO...!!!";
-      }
-    else
-     {
-        t = p;
-        while(t->sgte!=NULL)
-         {
-            t = t->sgte;
-          }
-        t->sgte = nuevo;
-        cout<<"NODO INGRESADO...!!!";
-      }
-
- }
-
-/*                      AGREGAR ARISTA
-    funcion que utilizada para agregar la arista a dos nodos
----------------------------------------------------------------------*/
-void agrega_arista(Tnodo &aux, Tnodo &aux2, Tarista &nuevo)
-{
-    Tarista q;
-    if(aux->ady==NULL)
-    {   aux->ady=nuevo;
-        nuevo->destino=aux2;
-        cout<<"PRIMERA ARISTA....!";
-    }
-    else
-    {   q=aux->ady;
-        while(q->sgte!=NULL)
-            q=q->sgte;
-        nuevo->destino=aux2;
-        q->sgte=nuevo;
-        cout<<"ARISTA AGREGADA...!!!!";
-    }
-
-}
-/*                      INSERTAR ARISTA
-    funcion que busca las posiciones de memoria de los nodos
-    y hace llamado a agregar_arista para insertar la arista
----------------------------------------------------------------------*/
-void insertar_arista()
-{   char ini, fin;
-    Tarista nuevo=new struct arista;
-    Tnodo aux, aux2;
-
-    if(p==NULL)
-     {
-         cout<<"GRAFO VACIO...!!!!";
-         return;
-     }
-    nuevo->sgte=NULL;
-    cout<<"INGRESE NODO DE INICIO:";
-    cin>>ini;
-    cout<<"INGRESE NODO FINAL:";
-    cin>>fin;
-    aux=p;
-    aux2=p;
-    while(aux2!=NULL)
-    {
-        if(aux2->nombre==fin)
-        {
-            break;
-        }
-
-        aux2=aux2->sgte;
-    }
-    while(aux!=NULL)
-    {
-        if(aux->nombre==ini)
-        {
-            agrega_arista(aux,aux2, nuevo);
-            return;
-        }
-
-        aux = aux->sgte;
-    }
-}
-
-/*          FUNCION PARA BORRAR TODAS LAS ARISTAS DE UN NODO
-    esta funcion es utilizada al borrar un nodo pues si tiene aristas
-    es nesesario borrarlas tambien y dejar libre la memoria
----------------------------------------------------------------------*/
-void vaciar_aristas(Tnodo &aux)
-{
-    Tarista q,r;
-    q=aux->ady;
-    while(q->sgte!=NULL)
-    {
-        r=q;
-        q=q->sgte;
-        delete(r);
-    }
-}
-/*                      ELIMINAR NODO
-    funcion utilizada para eliminar un nodo del grafo
-    pero para eso tambien tiene que eliminar sus aristas por lo cual
-    llama a la funcion vaciar_aristas para borrarlas
----------------------------------------------------------------------*/
-void eliminar_nodo()
-{   char var;
-    Tnodo aux,ant;
-    aux=p;
-    cout<<"ELIMINAR UN NODO\n";
-    if(p==NULL)
-     {
-         cout<<"GRAFO VACIO...!!!!";
-         return;
-     }
-    cout<<"INGRESE NOMBRE DE VARIABLE:";
-    cin>>var;
-
-    while(aux!=NULL)
-    {
-        if(aux->nombre==var)
-        {
-            if(aux->ady!=NULL)
-                vaciar_aristas(aux);
-
-            if(aux==p)
-            {
-
-                    p=p->sgte;
-                    delete(aux);
-                    cout<<"NODO ELIMINADO...!!!!";
-                    return;
-
-
-
-            }
-            else
-            {
-                ant->sgte = aux->sgte;
-                delete(aux);
-                cout<<"NODO ELIMINADO...!!!!";
-                return;
-            }
-        }
-        else
-        {
-            ant=aux;
-            aux=aux->sgte;
-         }
-    }
-
-}
-
-/*                      ELIMINAR ARISTA
-    funcion utilizada para eliminar una arista
----------------------------------------------------------------------*/
-void eliminar_arista()
-{
-char ini, fin;
-    Tnodo aux, aux2;
-    Tarista q,r;
-    cout<<"\n ELIMINAR ARISTA\n";
-    cout<<"INGRESE NODO DE INICIO:";
-    cin>>ini;
-    cout<<"INGRESE NODO FINAL:";
-    cin>>fin;
-    aux=p;
-    aux2=p;
-    while(aux2!=NULL)
-    {
-        if(aux2->nombre==fin)
-        {
-            break;
-        }
-        else
-        aux2=aux2->sgte;
-    }
-     while(aux!=NULL)
-    {
-        if(aux->nombre==ini)
-        {
-            q=aux->ady;
-            while(q!=NULL)
-            {
-                if(q->destino==aux2)
-                {
-                    if(q==aux->ady)
-                        aux->ady=aux->ady->sgte;
-                    else
-                        r->sgte=q->sgte;
-                    delete(q);
-                    cout<<"ARISTA  "<<aux->nombre<<"----->"<<aux2->nombre<<" ELIMINADA.....!!!!";
-                    return;
-                }
-            }
-            r=q;
-            q=q->sgte;
-        }
-        aux = aux->sgte;
-    }
-}
-/*                      MOSTRAR GRAFO
-    funcion que imprime un grafo en su forma enlazada
----------------------------------------------------------------------*/
-void mostrar_grafo()
-{   Tnodo ptr;
-    Tarista ar;
-    ptr=p;
-    cout<<"NODO|LISTA DE ADYACENCIA\n";
-
-    while(ptr!=NULL)
-    {   cout<<"   "<<ptr->nombre<<"|";
-        if(ptr->ady!=NULL)
-        {
-            ar=ptr->ady;
-            while(ar!=NULL)
-            {   cout<<" "<<ar->destino->nombre;
-                ar=ar->sgte;
-             }
-
-        }
-        ptr=ptr->sgte;
-        cout<<endl;
-    }
-}
-
-/*                      MOSTRAR ARISTAS
-    funcion que muestra todas las aristas de un nodo seleccionado
----------------------------------------------------------------------*/
-void mostrar_aristas()
-{
-    Tnodo aux;
-    Tarista ar;
-    char var;
-    cout<<"MOSTRAR ARISTAS DE NODO\n";
-    cout<<"INGRESE NODO:";
-    cin>>var;
-    aux=p;
-    while(aux!=NULL)
-    {
-        if(aux->nombre==var)
-        {
-            if(aux->ady==NULL)
-            {   cout<<"EL NODO NO TIENE ARISTAS...!!!!";
-                return;
-             }
-            else
-            {
-                cout<<"NODO|LISTA DE ADYACENCIA\n";
-                cout<<"   "<<aux->nombre<<"|";
-                ar=aux->ady;
-
-                while(ar!=NULL)
-                {
-                    cout<<ar->destino->nombre<<" ";
-                    ar=ar->sgte;
-                }
-                cout<<endl;
-                return;
-            }
-        }
-        else
-        aux=aux->sgte;
-    }
-}
-
-*/
